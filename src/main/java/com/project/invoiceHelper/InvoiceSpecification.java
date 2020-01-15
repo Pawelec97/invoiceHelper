34c814c8bd12d90d0edcb7f012f52e6d92cb1afd
Wisniewski, P <p.wisniewski@accenture.com>
2020-01-15 15:00:07 +0100

    wyjątek dla nulla w kryteriach

--- src/main/java/com/project/invoiceHelper/InvoiceSpecification.java
+++ src/main/java/com/project/invoiceHelper/InvoiceSpecification.java
@@ -8,16 +8,14 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class InvoiceSpecification implements Specification<Invoice> {
 
-	private List<SearchCriteria> criterias;
+	private List<SearchCriteria> criterias = new ArrayList<>();
 
 	public InvoiceSpecification() {
-		this.criterias = new ArrayList<>();
 	}
 
 	public void add(SearchCriteria criteria) {
@@ -31,38 +29,14 @@ public class InvoiceSpecification implements Specification<Invoice> {
 		List<Predicate> predicates = new ArrayList<>();
 
 		for (SearchCriteria criteria : criterias) {
-			if (criteria.getOperation().equals(SearchOperation.GREATER_THAN)) {
-				predicates.add(builder.greaterThan(
-						root.get(criteria.getKey()), criteria.getValue().toString()));
-			} else if (criteria.getOperation().equals(SearchOperation.LESS_THAN)) {
-				predicates.add(builder.lessThan(
-						root.get(criteria.getKey()), criteria.getValue().toString()));
-			} else if (criteria.getOperation().equals(SearchOperation.GREATER_THAN_EQUAL)) {
-				predicates.add(builder.greaterThanOrEqualTo(
-						root.get(criteria.getKey()), criteria.getValue().toString()));
-			} else if (criteria.getOperation().equals(SearchOperation.GREATER_THAN_EQUAL_LOCALDATE)) {
-				predicates.add(builder.greaterThanOrEqualTo(
-						root.get(criteria.getKey()), (LocalDate) criteria.getValue()));
-			} else if (criteria.getOperation().equals(SearchOperation.LESS_THAN_EQUAL)) {
-				predicates.add(builder.lessThanOrEqualTo(
-						root.get(criteria.getKey()), criteria.getValue().toString()));
-			} else if (criteria.getOperation().equals(SearchOperation.LESS_THAN_EQUAL_LOCALDATE)) {
-				predicates.add(builder.lessThanOrEqualTo(
-						root.get(criteria.getKey()), (LocalDate) criteria.getValue()));
-			} else if (criteria.getOperation().equals(SearchOperation.NOT_EQUAL)) {
-				predicates.add(builder.notEqual(
-						root.get(criteria.getKey()), criteria.getValue()));
-			} else if (criteria.getOperation().equals(SearchOperation.EQUAL)) {
-				predicates.add(builder.equal(
-						root.get(criteria.getKey()), criteria.getValue()));
-			} else if (criteria.getOperation().equals(SearchOperation.BETWEEN)) {
-				predicates.add(builder.between(
-						root.get(criteria.getKey()), (LocalDate) ((List) criteria.getValue()).get(0),
-						(LocalDate) ((List) criteria.getValue()).get(1)));
+			try {
+				predicates.add(criteria.getOperation().getPredicate(root, builder, criteria));
+			}catch(NullPointerException e){
+				System.out.println("bad Search Operation");
 			}
 		}
 
-		return builder.and(predicates.toArray(new Predicate[0]));
+		return builder.and(predicates.toArray(new Predicate[0])); //https://stackoverflow.com/questions/48842145/how-to-add-a-list-of-predicates-to-criteriabuilder-or
 
 	}
 }
--- src/main/java/com/project/invoiceHelper/SearchOperation.java
+++ src/main/java/com/project/invoiceHelper/SearchOperation.java
@@ -1,5 +1,12 @@
 package com.project.invoiceHelper;
 
+import com.project.invoiceHelper.entities.Invoice;
+import java.time.LocalDate;
+import javax.management.BadAttributeValueExpException;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
 public enum SearchOperation {
 	GREATER_THAN,
 	LESS_THAN,
@@ -8,6 +15,48 @@ public enum SearchOperation {
 	LESS_THAN_EQUAL,
 	LESS_THAN_EQUAL_LOCALDATE,
 	NOT_EQUAL,
-	EQUAL,
-	BETWEEN
+	EQUAL;
+
+	public Predicate getPredicate(Root<Invoice> root, CriteriaBuilder builder,
+			SearchCriteria criteria) {
+
+		switch (this) {
+			case GREATER_THAN: {
+				return builder.greaterThan(
+						root.get(criteria.getKey()), criteria.getValue().toString());
+			}
+			case LESS_THAN: {
+				return builder.lessThan(
+						root.get(criteria.getKey()), criteria.getValue().toString());
+			}
+			case GREATER_THAN_EQUAL: {
+				return builder.greaterThanOrEqualTo(
+						root.get(criteria.getKey()), criteria.getValue().toString());
+			}
+			case LESS_THAN_EQUAL: {
+				return builder.lessThanOrEqualTo(
+						root.get(criteria.getKey()), criteria.getValue().toString());
+			}
+			case EQUAL: {
+				return builder.equal(
+						root.get(criteria.getKey()), criteria.getValue());
+			}
+			case NOT_EQUAL: {
+				return builder.notEqual(
+						root.get(criteria.getKey()), criteria.getValue());
+			}
+			case LESS_THAN_EQUAL_LOCALDATE: {
+				return builder.lessThanOrEqualTo(
+						root.get(criteria.getKey()), (LocalDate) criteria.getValue());
+			}
+			case GREATER_THAN_EQUAL_LOCALDATE: {
+				return builder.greaterThanOrEqualTo(
+						root.get(criteria.getKey()), (LocalDate) criteria.getValue());
+			}
+			default: {
+				return null;
+			}
+		}
+
+	}
 }
